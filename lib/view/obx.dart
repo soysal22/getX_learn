bb82043280053d7bb266eb72e1602d62c797b0e7
ibrahim Soysal <ibrahim.soysal@ithinka.com>
2023-01-01 23:11:29 +0300

    getxLearm

---
+++ lib/view/obx.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http_get_use/core/service/todos_service.dart';
+
+class Obxxxx extends StatelessWidget {
+  const Obxxxx({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text("REST APİ"),
+          ),
+          body: ,
+          
+          
+          );  }
+}
